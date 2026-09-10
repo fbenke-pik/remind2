e531706574c41e9ded3170ae3a79d1d9bcbaa435
Falk Benke <falk.benke@pik-potsdam.de>
2026-09-10 11:46:20 +0200

    remove gdxrrw

--- tests/testthat/test-convGDX2mif.R
+++ tests/testthat/test-convGDX2mif.R
@@ -8,10 +8,8 @@
 # both ("all"). Sensitivity determines the allowed offset when comparing
 # LHS to RHS
 library(dplyr)
-library(gdx)
 
 test_that("Test if REMIND reporting is produced as it should and check data integrity", {
-  skip_if_not(as.logical(gdxrrw::igdx(silent = TRUE)), "gdxrrw is not initialized properly")
 
   # Raise the download timeout (default is 60s), as the reference GDXs are large (>100 MB)
   # and would otherwise time out on slower connections, aborting the build.
